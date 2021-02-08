6adb0651871b25147352164d6bce71a23230be6a
jonathandennis <4jondennis@gmail.com>
2021-02-08 16:19:00 +0100

    implemented code to dynamically load Landscape carousel

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const data = [
+export const data = [
     {
         id: 1,
         section: "Landscape",
@@ -23,9 +23,57 @@ const data = [
     {
         id: 4,
         section: "Landscape",
+        pic: "img/lge/landscape/TOTR_Sunset.jpg",
+        alt: "Southern view from Top of the Rock, NYC",
+        caption: "View from the Top of the Rock. Rockefeller Center, NYC",
+    },
+    {
+        id: 5,
+        section: "Landscape",
         pic: "img/lge/landscape/5points.jpg",
         alt: "5points Queens, NY",
         caption: "This no longer exists, a story of gentrification. 5Pointz, Long Island City Queens, NY for: Vice Magazine",
+    },
+    {
+        id: 6,
+        section: "Landscape",
+        pic: "img/lge/landscape/DJI_0075.jpg",
+        alt: "Brooklyn Navy Yard solar project from above",
+        caption: "Brooklyn Navy Yard Solar system built by: Solar Energy Systems",
+    },
+    {
+        id: 7,
+        section: "Landscape",
+        pic: "img/lge/landscape/AviatorColor.jpg",
+        alt: "Aviator rink Brooklyn, NY",
+        caption: "Aviator Sports Center. Floyd Bennett Field, Brooklyn",
+    },
+    {
+        id: 8,
+        section: "Landscape",
+        pic: "img/lge/landscape/Charlie.jpg",
+        alt: "Hiking in the White Mountain range",
+        caption: "Foggy Morning, White Mountain National Forest",
+    },
+    {
+        id: 9,
+        section: "Landscape",
+        pic: "img/lge/landscape/larahotrock.jpg",
+        alt: "Sunbather, Antigua, W.I.",
+        caption: "Sunbathing on the hot rock. Antigua W.I.",
+    },
+    {
+        id: 10,
+        section: "Landscape",
+        pic: "img/lge/landscape/oia2.jpg",
+        alt: "Oia SanTorini, Greece",
+        caption: "Sunset in Oia. San Torini, Greece",
+    },
+    {
+        id: 11,
+        section: "Landscape",
+        pic: "img/lge/landscape/hfscrowd.jpg",
+        alt: "HFStival front row",
+        caption: "All access pass, a view from the stage. WHFStival, RFK Stadium Washington D.C.",
     }
-]
-export default data;
+];
--- index.js
+++ index.js
@@ -1,33 +1,102 @@
-import data from "./data"
+const data = [
+    {
+        id: 1,
+        section: "Landscape",
+        pic: "img/lge/landscape/DJI_0006.jpg",
+        alt: "Redhook, Brooklyn",
+        caption: "The abandonded silo. Red Hook, Brooklyn",
+    },
+    {
+        id: 2,
+        section: "Landscape",
+        pic: "img/lge/landscape/DJI_0059.jpg",
+        alt: "Bruno Trucking sign. Gowanus, Brooklyn",
+        caption: "Bruno Trucking Sales sign. Gowanus, Brooklyn",
+    },
+    {
+        id: 3,
+        section: "Landscape",
+        pic: "img/lge/landscape/vanpac02.jpg",
+        alt: "Vanderbilt and Pacific Brooklyn, NY",
+        caption: "This no longer exists, a story of gentrification. Prospect Heights, Brooklyn",
+    },
+    {
+        id: 4,
+        section: "Landscape",
+        pic: "img/lge/landscape/TOTR_Sunset.jpg",
+        alt: "Southern view from Top of the Rock, NYC",
+        caption: "View from the Top of the Rock. Rockefeller Center, NYC",
+    },
+    {
+        id: 5,
+        section: "Landscape",
+        pic: "img/lge/landscape/5points.jpg",
+        alt: "5points Queens, NY",
+        caption: "This no longer exists, a story of gentrification. 5Pointz, Long Island City Queens, NY for: Vice Magazine",
+    },
+    {
+        id: 6,
+        section: "Landscape",
+        pic: "img/lge/landscape/DJI_0075.jpg",
+        alt: "Brooklyn Navy Yard solar project from above",
+        caption: "Brooklyn Navy Yard Solar system built by: Solar Energy Systems",
+    },
+    {
+        id: 7,
+        section: "Landscape",
+        pic: "img/lge/landscape/AviatorColor.jpg",
+        alt: "Aviator rink Brooklyn, NY",
+        caption: "Aviator Sports Center. Floyd Bennett Field, Brooklyn",
+    },
+    {
+        id: 8,
+        section: "Landscape",
+        pic: "img/lge/landscape/Charlie.jpg",
+        alt: "Hiking in the White Mountain range",
+        caption: "Foggy Morning, White Mountain National Forest",
+    },
+    {
+        id: 9,
+        section: "Landscape",
+        pic: "img/lge/landscape/larahotrock.jpg",
+        alt: "Sunbather, Antigua, W.I.",
+        caption: "Sunbathing on the hot rock. Antigua W.I.",
+    },
+    {
+        id: 10,
+        section: "Landscape",
+        pic: "img/lge/landscape/oia2.jpg",
+        alt: "Oia SanTorini, Greece",
+        caption: "Sunset in Oia. San Torini, Greece",
+    },
+    {
+        id: 11,
+        section: "Landscape",
+        pic: "img/lge/landscape/hfscrowd.jpg",
+        alt: "HFStival front row",
+        caption: "All access pass, a view from the stage. WHFStival, RFK Stadium Washington D.C.",
+    }
+]
 
-// Create HTML elements needed for each carousel image
-const figure = document.createElement('figure');
-const picture = document.createElement("picture");
-const img = document.createElement("img");
-const figcaption = document.createElement("figcaption");
 
-// The element where newly created elements will be appended to
-const div = document.querySelector("div");
+function imagesForCarousel(image) {
+    // Create HTML elements needed for each carousel image
+    const figure = document.createElement("figure");
+    const picture = document.createElement("picture");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    // Append newly created elements to the DOM
+    const landscapePort = document.querySelector('#landscapePort');
+    landscapePort.append(figure);
+    figure.append(picture);
+    picture.append(img);
+    figure.append(figcaption);
+    //Set content and attributes
+    img.setAttribute("class", 'portfolio__img');
+    img.setAttribute("src", image.pic);
+    img.setAttribute("alt", image.alt);
+    // add the caption to figcaption
+    figcaption.innerHTML = image.caption;
+}
 
-// Append the elements to the DOM
-
-// add figure to the div
-div.append(figure);
-// Add the picture tag inside of figure
-figure.append(picture);
-// Add the img inside the picture
-picture.append(img);
-// Add the figcaption inside the div
-div.append(figcaption);
-
-// use first image in data
-const image = data[0];
-
-// assign attributes to the elements
-img.setAttribute("class", 'portfolio__img');
-img.setAttribute("src", image.pic);
-img.setAttribute("alt", image.alt);
-// add the caption to figcaption
-figcaption.innerHTML = image.caption;
-
-export default script
+data.forEach(image => imagesForCarousel(image));
